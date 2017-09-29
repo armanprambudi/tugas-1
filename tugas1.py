8978919b4743208bb2f0dc2ac829ac9bc8e7ab33
armanprambudi <32378233+armanprambudi@users.noreply.github.com>
2017-09-29 10:20:51 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,8 @@
+namaku="MOHAMMAD ARMAN PRAMBUDI"
+nim="04316009"
+umurku= 19
+
+print("nama:"+namaku)
+print("nim:"+nim)
+print("umurku sekarang:", str(umurku),"tahun")
+print("umurku 5 tahun kedepan:",str(umurku+5),"tahun",str((umurku+5)*52),"minggu")
